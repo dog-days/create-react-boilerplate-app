098055817c9bf3141668ff8970efdb09b711aa9e
Sam <Sam@Sam-MacBook.local>
2017-05-24 18:17:58 +0800

    update tempate

--- packages/react-boilerplate-app-scripts/template/en_US-src/view/about/_route.js
+++ packages/react-boilerplate-app-scripts/template/en_US-src/view/about/_route.js
@@ -2,17 +2,17 @@
 //deal with prefix url
 import path from 'react-redux-boilerplate-js/libs/path'
 
+//react-router configuration,please refer to react-router@3.x.x.
 let routeConfig = {
-  //it's react-router's business.
-  //it' just for binding layout,when using `npm run ac`.
-  //it's only one palce different from react-router.
+  //It' just for binding layout,when using `npm run ac`.
+  //It's only one palce different from react-router.
   //refer to api.
   layout: 'main',
-  //url path setting
-  //because not all the web app is in web root dir,it might be in the root child dir.
-  //for example,PREFIX_URL = `/demo`，访问网站根目录demo文件中的web app
+  //Url path setting
+  //Because not all the web app is in web root dir,it might be in the root child dir.
+  //For example,PREFIX_URL = `/demo`，访问网站根目录demo文件中的web app
   //PREFIX_URL is read from package.json field 'react-boilerplate-app-scripts'
-  //if you do not use the prefix url function,the path can can be '/about'.
+  //If you do not use the prefix url function,the path can be '/about'.
   path: path('/about'),
   //code spliting
   getComponent(location, cb) {
--- packages/react-boilerplate-app-scripts/template/zh_CN-src/view/about/_route.js
+++ packages/react-boilerplate-app-scripts/template/zh_CN-src/view/about/_route.js
@@ -1,29 +1,28 @@
 'use strict';
-//deal with prefix url
+//处理prefix url
 import path from 'react-redux-boilerplate-js/libs/path'
 
+//route配置请参考react-router@3.x.x
 let routeConfig = {
-  //it's react-router's business.
-  //it' just for binding layout,when using `npm run ac`.
-  //it's only one palce different from react-router.
-  //refer to api.
+  //这是是为了绑定layout，当使用`npm run ac'时，会读取该值。
+  //这是唯一跟react-router不同的地方，是我们自定义的。 //refer to api.
   layout: 'main',
-  //url path setting
-  //because not all the web app is in web root dir,it might be in the root child dir.
-  //for example,PREFIX_URL = `/demo`，访问网站根目录demo文件中的web app
-  //PREFIX_URL is read from package.json field 'react-boilerplate-app-scripts'
-  //if you do not use the prefix url function,the path can can be '/about'.
+  //url路径设置
+  //因为不是所有的web app都是在web根目录，它可能在根目录的子文件夹中。
+  //例如，PREFIX_URL = `/demo`,访问网站根目录demo文件中的web app。
+  //PREFIX_URL读取与package.json的字段'react-boilerplate-app-scripts'。
+  //如果你使用prefix url功能，path可以是’/about‘。
   path: path('/about'),
-  //code spliting
+  //代码分离
   getComponent(location, cb) {
     require.ensure([], (require) => {
       cb(null, require('./index').default)
-    },'about')//code spliting name.
+    },'about')//代码分离命名.
   },
 }
-//react-hot-loader is not work with react-router code spliting.
-//so it is.
-//getComponent above will be covered.
+//热替换在react-router中代码分离并不生效。
+//所以就有了下面的代码。
+//上面的getComponent会被覆盖。
 if(module.hot) {
   routeConfig.component = require('./index').default;
 }
