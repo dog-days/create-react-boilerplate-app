78648fd9d5d90bbca3c6998f839944e4237c9d4d
Sam <Sam@Sam-MacBook.local>
2017-06-07 20:53:14 +0800

    fix fetch problem

--- packages/react-mobx-boilerplate-js/src/fetch.js
+++ packages/react-mobx-boilerplate-js/src/fetch.js
@@ -1,6 +1,6 @@
 /**
- * 获取fetch的默认配置 
- *@return { object } options 配置选项 
+ * 获取fetch的默认配置
+ *@return { object } options 配置选项
  *  options.bodyType是自定义新增的一项，发送信息的body体，
  *  一般分两种json数据和form data数据（上传文件时）,
  *  当然也可以或略这一项，安装fetch原来的写法传body数据也是可以的
@@ -9,7 +9,7 @@ export function getFetchOptions(options) {
   let opt = {
     bodyType: 'json',
     headers: {
-      Accept: 'application/json; charset=utf-8',
+      Accept: 'text/html,application/xhtml+xml,application/xml,application/json; charset=utf-8',
       'Content-Type': 'application/json; charset=utf-8',
     },
     //include允许跨域传cookie，same-origin允许同域传cookie
@@ -30,7 +30,7 @@ export function getFetchOptions(options) {
 
 /**
  * fetchOne 获取接口数据
- *@param { string } url 目标请求url 
+ *@param { string } url 目标请求url
  *@param { object } options 配置继承fetch中所有配置项的
  *@return { promise } 返回Promise对象，参数为请求的结果
  */
@@ -52,10 +52,10 @@ export function fetchOne(url, options) {
 }
 /**
  * fetchMore 获取多个接口数据
- *@param {array} urls 目标请求urls 
+ *@param {array} urls 目标请求urls
  *@param { array || object } options 配置组，跟urls索引对应，配置继承fetch中所有配置项的
  * 如果不是object类型，就属于全部urls的共同配置
- *@return { promise } 返回Promise对象，参数为所有请求的结果 
+ *@return { promise } 返回Promise对象，参数为所有请求的结果
  */
 export function fetchMore(urls, options) {
   var _urls = [];
