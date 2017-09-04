744686454c9ddce4c1f0466b225e9c930952d9f9
xianshannan <xianshannan@qq.com>
2017-09-04 14:56:14 +0800

    add template/simple

---
+++ packages/react-boilerplate-app-scripts/template/simple/container.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from 'src/assets/img/logo.svg';
+import 'src/assets/css/container.css';
+
+class Container extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <div className="app-header">
+          <img src={logo} className="app-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="app-intro">
+          请编辑src/container.jsx，保存后页面会自动更新编辑后内容，无需手动刷新页面。
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Container;
