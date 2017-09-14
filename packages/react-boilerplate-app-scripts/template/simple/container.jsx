5b0794a4bdc8e908bfe213ec1676a5ffe7b16787
xianshannan <xianshannan@qq.com>
2017-09-14 16:48:00 +0800

    update template

--- packages/react-boilerplate-app-scripts/template/simple/container.jsx
+++ packages/react-boilerplate-app-scripts/template/simple/container.jsx
@@ -8,7 +8,7 @@ class Container extends React.Component {
       <div className="app">
         <div className="app-header">
           <img src={logo} className="app-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>欢迎使用React</h2>
         </div>
         <p className="app-intro">
           请编辑src/container.jsx，保存后页面会自动更新编辑后内容，无需手动刷新页面。
